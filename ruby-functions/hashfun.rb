84f5c4e0aeb7be005883a9461fc67c96640b3aa7
TonyWakuu <any@wakuu.enterprises>
2019-05-07 16:32:05 -0700

    updated

--- ruby-functions/hashfun.rb
+++ ruby-functions/hashfun.rb
@@ -1,6 +1,6 @@
 require 'rspec'
 
-class Array
+class String
     def index_hash
         self.each_with_object({}) do |item, hash|
             hash[self.index(item)] = item
@@ -8,9 +8,9 @@ class Array
     end 
 end
 
-describe 'Array to Hash converter' do
+describe 'String to Hash converter' do
     it 'converts an array of strings to a hash, with the keys being the index and the value being the element' do
-        arr = %w{key:[[value_1, value_2],[value_3, value4]], 5:10:00AM]}
-        expect(arr.index_hash).to eq({"key" => [["value_1", "value_2"],["value_3", "value4"]], 5=>"10:00AM"})
+        str = %w{key:[[value_1, value_2],[value_3, value4]], 5:10:00AM]}
+        expect(str.index_hash).to eq({"key" => [["value_1", "value_2"],["value_3", "value4"]], 5=>"10:00AM"})
     end
 end
--- ruby-functions/zerocounter.js
+++ ruby-functions/zerocounter.js
@@ -1,24 +1,24 @@
 function zeros_count(n) {
 var val = "";
-var zeros = 0;
-const count = occur(val, 0);
 
     while(n > 0) {
         val = n%2 + val;
         n = Math.floor(n/2);
     }
-
-    // function occur(int, number) {
-    //     let count = 0;
-    //     for (let int of int)
-    //         if (int === number)
-    //             count++;
-    //         return count;
-    // }
-
-
+    int = Array.from(val.toString()).map(Number);
+    function occur(array, number) {
+        let count = 0;
+        for (let element of array)
+            if (element === number) {
+                count++;
+            }
+            return count;
+    }
+    count = occur(int, 0);
+    
 return val;
 }
 
-console.log(zeros_count(16))
-// console.log(count)
+console.log(zeros_count(42))
+console.log(count)
+console.log(int)
